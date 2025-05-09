c00f157b79bbf5dbfb41e68877e5891a5d6c54de
Devarsh <devarsh.chhatrala@simformsolutions.com>
2025-05-09 18:43:42 +0530

    Handled CORS Error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from typing import Annotated
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from src.todos.controller import router as todo_router
 from src.auth.controller import router as auth_router
 from src.database import Base, engine
@@ -10,6 +11,15 @@ app = FastAPI()
 
 app.include_router(todo_router)
 app.include_router(auth_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def main():
